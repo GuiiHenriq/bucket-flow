190c300186e827a05a015af2f8267b46144e1735
GuiiHenriq <guilhermeerba@hotmail.com>
2025-04-26 11:51:49 -0300

    :card_file_box: raw: Add v1.0 Schema GraphQL

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,21 @@ import Router from "@koa/router";
 import bodyParser from "koa-bodyparser";
 import logger from "koa-logger";
 import { ApolloServer } from "apollo-server-koa";
+import { typeDefs, resolvers } from "./graphql/schema";
 import { authMiddleware } from "./middlewares/auth";
+import { routes } from "./routes";
 
 const app = new Koa();
 
 const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
   context: ({ ctx }) => {
     return { user: ctx.state.user };
   },
 });
 
+// Middleware
 app.use(logger());
 app.use(bodyParser());
 
