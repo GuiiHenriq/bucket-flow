a7dfd08fac0f130324b9a9365cf2434339269257
GuiiHenriq <guilhermeerba@hotmail.com>
2025-05-09 23:00:57 -0300

    :sparkles: feat: Add admin panel screen

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,30 +1,31 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'type-enum': [
+    "type-enum": [
       2,
-      'always',
+      "always",
       [
-        ':books: docs',
-        ':bug: fix',
-        ':sparkles: feat',
-        ':bricks: ci',
-        ':recycle: refactor',
-        ':zap: perf',
-        ':boom: fix',
-        ':lipstick: feat',
-        ':test_tube: test',
-        ':bulb: docs',
-        ':card_file_box: raw'
-      ]
+        ":books: docs",
+        ":bug: fix",
+        ":sparkles: feat",
+        ":bricks: ci",
+        ":recycle: refactor",
+        ":zap: perf",
+        ":boom: fix",
+        ":lipstick: feat",
+        ":test_tube: test",
+        ":bulb: docs",
+        ":card_file_box: raw",
+        ":wrench: chore",
+      ],
     ],
-    'type-case': [0],
-    'type-empty': [0],
-    'scope-empty': [0],
-    'scope-case': [0],
-    'subject-case': [0],
-    'subject-empty': [0],
-    'subject-full-stop': [0],
-    'header-max-length': [0]
-  }
-}; 
+    "type-case": [0],
+    "type-empty": [0],
+    "scope-empty": [0],
+    "scope-case": [0],
+    "subject-case": [0],
+    "subject-empty": [0],
+    "subject-full-stop": [0],
+    "header-max-length": [0],
+  },
+};
